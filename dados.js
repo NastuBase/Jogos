81ad09e18152f595b7870e3c6861718bccaedf8d
NastuBase <leandrograffunder8@gmail.com>
2024-09-06 20:09:25 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo : "Faker",
+        descricao : "Faker: O Rei Imortal do League of Legends, Faker é o apelido de Lee Sang-hyeok, um jogador profissional sul-coreano de League of Legends, considerado por muitos o melhor jogador da história do jogo. Ele é conhecido por sua mecânica impecável, game sense excepcional e liderança dentro da equipe.",
+        link : "https://pt.wikipedia.org/wiki/Faker_(jogador)",
+        posicao : "Midlaner, mid, goat, meio"
+    
+    
+    }, 
+    {
+        titulo : "Mylon",
+        descricao : "Mylon: A lenda brasileira do League of Legends. Mylon foi um jogador profissional brasileiro que marcou época defendendo a equipe Pain Gaming. Conhecido por seu estilo de jogo agressivo e habilidade com diversos campeões, ele conquistou o coração dos fãs brasileiros.",
+        link : "https://lol.fandom.com/wiki/Mylon",
+        posicao : "Toplaner, top, topo"
+    
+    
+    }, 
+    {
+        titulo : "Caps",
+        descricao : "Caps: O mid laner europeu que domina o cenário competitivo. Caps é um jogador profissional de League of Legends, atualmente jogando pela G2 Esports. Conhecido por sua mecânica impecável e game sense excepcional, ele é considerado um dos melhores mid laners do mundo.",
+        link : "https://en.wikipedia.org/wiki/Caps_(gamer)",
+        posicao : "Midlaner, mid, meio"
+    
+    
+    }
+];
+
